8568b90fe7a7aa48a0b562875044efcae46b2ba4
Bulat Kamalov <kamalovmarket@gmail.com>
2022-03-27 10:12:24 +0300

    Initial Commit

---
+++ NewSceneCleanSwiftArhitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewSceneCleanSwiftArhitecture
+//
+//  Created by Bulat Kamalov on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
